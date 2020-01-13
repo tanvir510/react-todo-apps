e9c48d10f0d4cd5d100f5897575e623646325978
tanvir510 <tanvir.ahammad4001@gmail.com>
2020-01-13 18:14:29 +0600

    portfolio data  code here

--- src/component/forms/PortfolioForm.js
+++ src/component/forms/PortfolioForm.js
@@ -1,155 +1,81 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+const base_url = "https://jsonplaceholder.typicode.com";
 
 class PortfolioForm extends Component {
   state = {
-    name: "",
-    email: "",
-    password: "",
-    bio: "",
-    skills: []
+    title: "",
+    body: "",
+    userId: 112,
+    isSuccess: false,
+    isError: false
   };
+
   changeHandler = e => {
-    if (e.target.type === "checkbox") {
-      if (e.target.checked) {
-        this.setState({
-          ...this.state,
-          skills: this.state.skills.concat(e.target.value)
-        });
-      } else {
-        this.setState({
-          ...this.state,
-          skills: this.state.skills.filter(skill => {
-            skill !== e.target.value;
-          })
-        });
-      }
-    } else {
-      this.setState({
-        [e.target.name]: e.target.value
-      });
-    }
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   };
   submitHandler = e => {
     e.preventDefault();
-    this.setState({
-      name: "",
-      email: "",
-      password: "",
-      bio: "",
-      skills: []
-    });
-    console.log(this.state);
+    axios
+      .post(`${base_url}/posts`, {
+        title: this.state.title,
+        body: this.state.body,
+        userId: this.state.userId
+      })
+      .then(res => {
+        this.setState({
+          body: "",
+          title: "",
+          isSuccess: true,
+          isError: false
+        });
+      })
+      .catch(error => {
+        this.setState({
+          isError: true
+        });
+      });
   };
   render() {
     return (
-      <form className="contact_form" onSubmit={this.submitHandler}>
-        <div className="form-group">
-          <label htmlFor="name">Your Name Here</label>
+      <form
+        className="form_data"
+        style={{ paddingTop: "25px" }}
+        onSubmit={this.submitHandler}
+      >
+        <div className="form-groups">
+          <label htmlFor="title">Form Title</label>
           <input
             type="text"
+            name="title"
+            id="title"
+            placeholder="Enter your title..."
             className="form-control"
-            id="name"
-            placeholder="Enter name..."
-            name="name"
             onChange={this.changeHandler}
-            value={this.state.name}
+            value={this.state.title}
           />
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Your Name Here</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            placeholder="Enter your email..."
-            name="email"
-            onChange={this.changeHandler}
-            value={this.state.email}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Your Name Here</label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Enter your password..."
-            name="password"
-            onChange={this.changeHandler}
-            value={this.state.password}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Your Name Here</label>
+        <div className="form-groups" style={{ paddingTop: "15px" }}>
+          <label htmlFor="body">Form Body</label>
           <textarea
-            style={{ height: "150px" }}
-            type="bio"
+            style={{ height: "120px" }}
+            type="text"
+            name="body"
+            id="body"
+            placeholder="Enter your body..."
             className="form-control"
-            id="bio"
-            placeholder="Enter your bio..."
-            name="bio"
             onChange={this.changeHandler}
-            value={this.state.bio}
+            value={this.state.body}
           />
         </div>
-        <div className="form-group">
-          <label className="skills_title"> Choose your skills:</label>
-          <div className="form-group form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="react"
-              name="skills"
-              value="react"
-              onChange={this.changeHandler}
-            />
-            <label className="form-check-label" htmlFor="react">
-              React
-            </label>
-          </div>
-          <div className="form-group form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="node"
-              name="skills"
-              value="node"
-              onChange={this.changeHandler}
-            />
-            <label className="form-check-label" htmlFor="node">
-              Node JS
-            </label>
-          </div>
-          <div className="form-group form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="vue"
-              name="skills"
-              onChange={this.changeHandler}
-              value="vue"
-            />
-            <label className="form-check-label" htmlFor="vue">
-              Vue JS
-            </label>
-          </div>
-          <div className="form-group form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="angular"
-              name="skills"
-              value="angular"
-              onChange={this.changeHandler}
-            />
-            <label className="form-check-label" htmlFor="angular">
-              Angular JS
-            </label>
-          </div>
-        </div>
-        <button className="btn btn-primary" type="submit">
+        <button className="btn btn-success" style={{ marginTop: "15px" }}>
           Submit
         </button>
+        {this.state.isSuccess && <p>Form data posted successfully</p>}
+        {this.state.isError && <p>Something wrong in your code!</p>}
       </form>
     );
   }
